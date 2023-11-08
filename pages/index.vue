6bc503b0a779f22f3ae586a0aff11329e9f5ace5
Rustom Pedales <pedales.rustom@gmail.com>
2023-11-08 22:02:28 +0800

    8-11-2023

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,17 @@
 <script setup>
-    const headers = [
-        { text: "PLAYER", value: "player" },
-        { text: "TEAM", value: "team" },
-        { text: "NUMBER", value: "number" },
-        { text: "POSITION", value: "position" },
-        { text: "HEIGHT", value: "indicator.height" },
-        { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
-        { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
-        { text: "COUNTRY", value: "country" },
-    ]
 
-    const items = [
-        { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: { "height": '6-2', "weight": 185 }, lastAttended: "Davidson", country: "USA" },
-        { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: { "height": '6-9', "weight": 250 }, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA" },
-        { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: { "height": '6-10', "weight": 240 }, lastAttended: "Texas-Austin", country: "USA" },
-        { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: { "height": '6-11', "weight": 242 }, lastAttended: "Filathlitikos", country: "Greece" },
-    ]
 </script>
 
-
 <template>
-    <RContainer container-label="Sportsman">
-        <RTable :tbl-headers="headers" :tbl-items="items"/>
+    <RContainer container-label="SUMMARY THIS MONTH">
+        <HrmsDashboardContentSummary/>
     </RContainer>
-    <RButton btn-label="Sample"/>
-    <RSearch />
-    <RContainer container-label="LEETCARD">
-        <div class="md:flex gap-2" >
-            <RSummaryCard card-label="TOTAL BIRTHDAYS" card-total-summary="3000">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
-                </svg>
-            </RSummaryCard>
-            <RSummaryCard card-label="TOTAL BIRTHDAYS" card-total-summary="3000">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
-                </svg>
-            </RSummaryCard>
-            <RSummaryCard card-label="TOTAL BIRTHDAYS" card-total-summary="3000">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
-                </svg>
-            </RSummaryCard>
-            <RSummaryCard card-label="TOTAL BIRTHDAYS" card-total-summary="3000">
-                <svg class="w-5 h-5 text-gray-80" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 17V2a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M5 15V1m8 18v-4"/>
-                </svg>
-            </RSummaryCard>
-        </div>
-
-    </RContainer>
+    <div class="md:flex gap-4">
+        <RContainer class="w-full md:w-1/2" container-label="BIRTHDAYS">
+            <HrmsDashboardContentBirthday/>
+        </RContainer>
+        <RContainer class="w-full md:w-1/2" container-label="EMPLOYEE CHART">
+            <HrmsDashboardContentChart/>
+        </RContainer>
+    </div>
 </template>
-
-<style>
-</style>
